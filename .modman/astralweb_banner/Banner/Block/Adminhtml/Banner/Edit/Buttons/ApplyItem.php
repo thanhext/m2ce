09a0ad28a6d3a3d190652601abac3077e4ef035a
Thanh Nguyen Van <thanhnv@ecommage.com>
2018-11-16 18:38:40 +0700

    develop: update ui form banner

---
+++ .modman/astralweb_banner/Banner/Block/Adminhtml/Banner/Edit/Buttons/ApplyItem.php
@@ -0,0 +1,10 @@
+<?php
+namespace AstralWeb\Banner\Block\Adminhtml\Banner\Edit\Buttons;
+/**
+ * Class ApplyItem
+ * @package AstralWeb\Banner\Block\Adminhtml\Banner\Edit\Buttons
+ */
+class ApplyItem extends \Magento\Backend\Block\Template
+{
+   public function _prepareLayout(){}
+}
