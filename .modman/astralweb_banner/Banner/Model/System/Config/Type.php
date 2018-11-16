c62b3c9df474dffabc57bfd84e1d85d83ff81141
Thanh Nguyen Van <thanhnv@ecommage.com>
2018-11-16 16:07:37 +0700

    develop: update ext banner

--- .modman/astralweb_banner/Banner/Model/System/Config/Type.php
+++ .modman/astralweb_banner/Banner/Model/System/Config/Type.php
@@ -12,11 +12,11 @@ class Type implements \Magento\Framework\Option\ArrayInterface
 
     public function toOptionArray()
     {
-        return [['value' => self::TYPE_SLIDER, 'label' => __('Slider')], ['value'=> self::TYPE_BANNER, 'label'=>__('Banner')]];
+        return [['value' => self::TYPE_SLIDER, 'label' => __('slides')], ['value'=> self::TYPE_BANNER, 'label'=>__('Banner')]];
     }
 
     public function toArray()
     {
-        return [ self::TYPE_SLIDER => __('Slider'), self::TYPE_BANNER => __('Banner')];
+        return [ self::TYPE_SLIDER => __('slides'), self::TYPE_BANNER => __('Banner')];
     }
 }
